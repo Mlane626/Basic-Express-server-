00bcf311be4c5e11eeb330e1f9e14597f909ad3d
Mike L <mike.lane.626@gmail.com>
2023-01-19 16:22:27 -0800

    Initial commit

--- server/app.js
+++ server/app.js
@@ -1,10 +1,31 @@
 // import files and packages up here
+const express = require('express');
+const morgan = require('morgan');
+const data = require('./data.json');
+const app = express();
+const port = 3000;
 
 
 // create your express server below
-var app;
+app.get('/', function (req, res){
+    res.send('Its working');
+    res.status(200).json(req.body);
+    res.send(data);
+});
 
+app.get('/data', function(req, res){
+    res.status(200);
+    res.send(data);
+})
 // add your routes and middleware below
+app.use(morgan('dev'));
+
 
 // finally export the express application
 module.exports = app;
+
+
+
+// form.on ('end', function() {
+//    res.writeHead(200, {'Content-Type': 'text/plain'});--- not usable bc npm test doesnt work 
+// });
